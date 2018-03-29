e27e0376c75dacaec390f7d4ec4d27349d458671
Andrés Ávila <andpy.me@gmail.com>
2018-03-28 23:04:23 -0600

    basic naive layout with Bulma

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
+import React, { Component } from 'react';
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+import SimpleStorageContract from '../build/contracts/SimpleStorage.json';
+import getWeb3 from './utils/getWeb3';
+
 
 class App extends Component {
   constructor(props) {
@@ -28,7 +25,7 @@ class App extends Component {
       })
 
       // Instantiate contract once web3 provided.
-      this.instantiateContract()
+      // this.instantiateContract()
     })
     .catch(() => {
       console.log('Error finding web3.')
@@ -69,23 +66,220 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
+      <div>
+        <section className="hero is-primary is-medium">
+          <div className="hero-head">
+            <nav className="navbar">
+              <div className="container">
+                <div className="navbar-brand">
+                  <a className="navbar-item">
+                    CARMED
+                  </a>
+                  <span className="navbar-burger burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </div>
+                <div className="navbar-menu">
+                  <div className="navbar-end">
+                    <a className="navbar-item is-active">
+                      Home
+                    </a>
+                    <a className="navbar-item">
+                      About
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
 
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">
+                Cartilla Médica
+              </h1>
+              <h2 className="subtitle">
+                Tu cartilla médica distribuida
+              </h2>
             </div>
           </div>
-        </main>
+        </section>
+
+        <section className="section container">
+          <div className="columns">
+            <div className="column">
+              <h4 className="title is-4">Llave Pública</h4>
+
+              <div className="field">
+                <div className="control">
+                  <textarea className="textarea is-primary" type="text" placeholder="Ejemplo: 0xC2D7CF95645D33006175B78989035C7c9061d3F9"></textarea>
+                </div>
+              </div>
+
+              <div className="control">
+                <button className="button is-primary">Submit</button>
+              </div>
+            </div>
+
+            <div className="column">
+              <h4 className="title is-4">Llave Privada</h4>
+
+              <div className="field">
+                <div className="control">
+                  <textarea className="textarea" type="text" placeholder="Ejemplo: 3a1076bf45ab87712ad64ccb3b10217737f7faacbf2872e88fdd9a537d8fe266"></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="section container">
+          <div className="tile box">
+            <div className="content container">
+
+              <h2 className="title is-2">Juan Perez Rodriguez</h2>
+              <h3 className="subtitle is-6">Ultima modificación: <em>05/Febrero/2018</em></h3>
+
+              <div className="is-pulled-right">
+                <div className="buttons has-addons">
+                  <button className="button">
+                    <span className="icon">
+                      <i className="fas fa-print"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
+
+              <table className="table is-hoverable">
+                <tbody>
+                  <tr>
+                    <th className="has-text-centered" colSpan="3">
+                      Información Básica
+                    </th>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Edad
+                    </th>
+                    <td>
+                      46
+                    </td>
+                    <td></td>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Sexo
+                    </th>
+                    <td>
+                      Masculino
+                    </td>
+                    <td></td>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Peso
+                    </th>
+                    <td>
+                      76 kg
+                    </td>
+                    <td>
+                      <button className="button is-small">
+                        <span className="icon is-small">
+                          <i className="fas fa-edit"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Estatura
+                    </th>
+                    <td>
+                      179 cm
+                    </td>
+                    <td>
+                      <button className="button is-small">
+                        <span className="icon is-small">
+                          <i className="fas fa-edit"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-centered" colSpan="3">
+                      Información Médica
+                    </th>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Alergias
+                    </th>
+                    <td>
+                      <ul>
+                        <li>Penicilina</li>
+                        <li>Sulfamidas</li>
+                      </ul>
+                    </td>
+                    <td>
+                      <button className="button is-small">
+                        <span className="icon is-small">
+                          <i className="fas fa-edit"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Padecimientos
+                    </th>
+                    <td>
+                      <ul>
+                        <li>Miopía</li>
+                        <li>Diabetes</li>
+                      </ul>
+                    </td>
+                    <td>
+                      <button className="button is-small">
+                        <span className="icon is-small">
+                          <i className="fas fa-edit"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th className="has-text-right">
+                      Cirugías
+                    </th>
+                    <td>
+                      <ul>
+                        <li>Arstrocopia de rodilla</li>
+                        <li>Extirpación de apendice</li>
+                      </ul>
+                    </td>
+                    <td>
+                      <button className="button is-small">
+                        <span className="icon is-small">
+                          <i className="fas fa-edit"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
